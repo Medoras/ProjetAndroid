673f6dde7bdb9ba247764190b45168fdcee2aa52
Antoine R <antoine.rochard1@gmail.com>
2017-06-15 12:20:05 +0200

    Bouton + index de l'appli

--- app/src/main/java/com/iutvel/legigan_rochard/Surface.java
+++ app/src/main/java/com/iutvel/legigan_rochard/Surface.java
@@ -5,15 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Surface extends AppCompatActivity  {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void redirection_surface(View view){
-        startActivity(new Intent(this, Surface.class));
+        setContentView(R.layout.surface);
     }
 }
